7ab08fedb5dfb34b4a7902a65493cce78cfb2082
rach liu rachliu@ucla.edu <rachliu@tetracosa.cs.ucla.edu>
2019-06-03 21:01:24 -0700

    add basic verif script
    Change-Id: I86a7506513cd1d9223d8df255f8117eccce134c4

---
+++ verif.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+src=lfsr
+cpu=atomic
+
+itb=system.cpu.itb
+dtb=system.cpu.dtb
+
+sysvars=(final_tick sim_ticks system.cpu.Branches)
+imemvars=($itb.rdAccesses $itb.wrAccesses)
+dmemvars=($dtb.rdAccesses $dtb.wrAccesses)
+
+
+get-lines () {
+  local file="$1" && shift
+  local vars=($@)
+  for w in ${vars[@]}; do
+    cat $file | grep "$w"
+  done
+}
+
+get-truth () {
+  get-lines "259tests/m5out-$src-$cpu/stats.txt" $@
+}
+
+get-test () {
+  get-lines "m5out/stats.txt" $@
+}
+
+compare () {
+  echo -e "--------------------"
+  echo -e "Ground truth:\n"
+
+  get-truth "${sysvars[@]}"
+  get-truth "${imemvars[@]}"
+  get-truth "${dmemvars[@]}"
+
+  echo -e "--------------------"
+  echo -e "Test:\n"
+
+  get-test "${sysvars[@]}"
+  get-test "${imemvars[@]}"
+  get-test "${dmemvars[@]}"
+}
+
+
+
+compare
+
+
