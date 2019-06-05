9f7953830045ae678b22cb02c0a2ebfc7d248bc0
rach liu rachliu@ucla.edu <rachliu@tetracosa.cs.ucla.edu>
2019-06-04 18:05:08 -0700

    update verif with new stats
    Change-Id: I610698dcf53a021571a6ed62425d21de6273a005

--- verif.sh
+++ verif.sh
@@ -7,9 +7,21 @@ cpu=derivO3
 itb=system.cpu.itb
 dtb=system.cpu.dtb
 
-sysvars=(final_tick sim_ticks system.cpu.Branches)
-imemvars=($itb.rdAccesses $itb.wrAccesses)
-dmemvars=($dtb.rdAccesses $dtb.wrAccesses)
+sysvars=(final_tick sim_ticks system.cpu.Branches system.cpu.ipc)
+memvars=(
+system.l2.overall_hits::.cpu.inst
+system.l2.overall_hits::.cpu.data
+system.l2.overall_hits::total
+system.l2.overall_miss_latency::.cpu.inst
+system.l2.overall_miss_latency::.cpu.data
+system.l2.overall_miss_latency::total
+system.l2.overall_miss_rate::.cpu.inst
+system.l2.overall_miss_rate::.cpu.data
+system.l2.overall_miss_rate::total
+system.l2.overall_misses::.cpu.inst
+system.l2.overall_misses::.cpu.data
+system.l2.overall_misses::total
+)
 
 
 get-lines () {
@@ -33,15 +45,14 @@ compare () {
   echo -e "Ground truth:\n"
 
   get-truth "${sysvars[@]}"
-  get-truth "${imemvars[@]}"
-  get-truth "${dmemvars[@]}"
+  get-truth "${memvars[@]}"
 
+  echo -e "\n\n"
   echo -e "--------------------"
   echo -e "Test:\n"
 
   get-test "${sysvars[@]}"
-  get-test "${imemvars[@]}"
-  get-test "${dmemvars[@]}"
+  get-test "${memvars[@]}"
 }
 
 
