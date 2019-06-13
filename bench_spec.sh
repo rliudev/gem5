ed56b467d3f3f33daa3bb4c3c2c5497e802960d8
rach liu rachliu@ucla.edu <rachliu@tetracosa.cs.ucla.edu>
2019-06-13 16:11:35 -0700

    fix bench-spec.sh
    Change-Id: I5e4624eebd938b12ee6b1a6fb857a7f68f3b724b

--- bench_spec.sh
+++ bench_spec.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-outdir="~/output/run1"
-mkdir -p $outdir
+
+GEM5HOME="$HOME/finalproject/gem5"
+SPECHOME="$HOME/cpu2017"
+
+outdir="$HOME/output/run1"
 
 pf=TaggedPrefetcher
 cpu=DerivO3CPU
 
-cmd=./build/X86/gem5.opt
-config=configs/example/se.py
+cmd=$GEM5HOME/build/X86/gem5.opt
+config=$GEM5HOME/configs/example/se.py
 
 build=victor_jun10-m64
 basedir=run/run_base_refrate_"$build".0000/
 tests=(perlbench gcc mcf omnetpp xalancbmk x264 leela exchange2)
 
 
+mkdir -p $outdir
+cd $SPECHOME && source shrc
 
 
 run_bench() {
@@ -23,7 +28,7 @@ run_bench() {
 
   arg="--caches  --l2cache --cmd=$exe  --l1i_size=16kB  --l1d_size=64kB --l2_size=256kB --cpu-type=$cpu  --mem-type=DDR3_1600_8x8  --cpu-clock=1GHz --maxinsts=10000000"
 
-  $cmd $config $arg --l2-hwp-type=$pf --mlu_pf_type=perceptron
+  $cmd $config $arg $opts --l2-hwp-type=$pf --mlu_pf_type=perceptron
 }
 
 
@@ -44,8 +49,12 @@ run() {
 
   go $t
   cd $basedir
-  run_$1()
-  cp -a m5out/. $outdir/$t
+
+  if [ "$t" = "gcc" ]; then
+    run_gcc
+  fi
+
+  cp -a m5out/. "$outdir/$t"
 }
 
 
