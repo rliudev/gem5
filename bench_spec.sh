eb4406f788cb46aa0ee99622f9ca7b443c1b9c5e
rach liu rachliu@ucla.edu <rachliu@tetracosa.cs.ucla.edu>
2019-06-13 16:32:01 -0700

    fix bench-spec.sh
    Change-Id: Ia26a2d17a31adac5e493a6710d1126da5f71be36

--- bench_spec.sh
+++ bench_spec.sh
@@ -28,13 +28,14 @@ run_bench() {
 
   arg="--caches  --l2cache --cmd=$exe  --l1i_size=16kB  --l1d_size=64kB --l2_size=256kB --cpu-type=$cpu  --mem-type=DDR3_1600_8x8  --cpu-clock=1GHz --maxinsts=10000000"
 
-  $cmd $config $arg $opts --l2-hwp-type=$pf --mlu_pf_type=perceptron
+  bash -c "$cmd $config $arg $opts --l2-hwp-type=$pf --mlu_pf_type=perceptron"
 }
 
 
 
 run_gcc() {
-  run_bench "gcc" "cpugcc_r_base.$build" --options=\"ref32.c -O3 -fselective-scheduling -fselective-scheduling2 -o ref32.opts-O3_-fselective-scheduling_-fselective-scheduling2.s\"
+  gcc_opts="--options=\"ref32.c -O3 -fselective-scheduling -fselective-scheduling2 -o ref32.opts-O3_-fselective-scheduling_-fselective-scheduling2.s\""
+  run_bench "gcc" "cpugcc_r_base.$build" "$gcc_opts"
 }
 
 
